a7c91f2abc7f62cfbae473ea7955c7b91f63b880
juniejung <jooeunjung0429@gmail.com>
2023-09-27 15:45:51 +0900

    프로젝트 구조

---
+++ src/main/java/aix/project/chatez/ChatezApplication.java
@@ -0,0 +1,13 @@
+package aix.project.chatez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatezApplication.class, args);
+	}
+
+}
