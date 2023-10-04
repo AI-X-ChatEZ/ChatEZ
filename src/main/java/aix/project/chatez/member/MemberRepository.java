d2ed3a3443c9b43b54eda71f686670f308aaf8f2
juniejung <jooeunjung0429@gmail.com>
2023-10-04 11:34:13 +0900

    회원가입 구현중

--- src/main/java/aix/project/chatez/member/MemberRepository.java
+++ src/main/java/aix/project/chatez/member/MemberRepository.java
@@ -1,6 +1,8 @@
 package aix.project.chatez.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 }
