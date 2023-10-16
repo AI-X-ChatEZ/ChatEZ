f35e64ee7f9bf4cd4d6fec531683c250e9b07292
DESKTOP-GSVPP71\hi <hhh7441@naver.com>
2023-10-16 12:01:04 +0900

    gitignore 재설정

--- src/main/java/aix/project/chatez/member/MyServiceRepository.java
+++
@@ -1,9 +0,0 @@
-package aix.project.chatez.member;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface MyServiceRepository extends JpaRepository<MyService, Long> {
-    List<MyService> findByMemberMemberNo(Long memberNo);
-}
